d95c04cdcb36133af53e59fa9d31cb78cd205619
Mohamed Zribi <86979984+MohamedZribi@users.noreply.github.com>
2023-02-23 09:48:14 +0100

    Create grid and header
    Create a 16x16 grid and header

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+// creating a grid
+const container = document.getElementById("grid");
+function makeRows(rows, cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    container.appendChild(cell).className = "grid-item";
+    cell.setAttribute('style', 'background-color: blue;'); 
+
+    cell.addEventListener('mouseenter', () => {
+        cell.setAttribute('style', 'background-color: red;');    
+      });
+       
+
+  };
+};
+
+makeRows(16, 16);
+
+
+
+
+
+
+
