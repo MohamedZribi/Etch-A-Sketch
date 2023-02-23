877370707f7dd91e2a9d2bfae99c84c3f1dd9d52
Mohamed Zribi <86979984+MohamedZribi@users.noreply.github.com>
2023-02-23 12:57:03 +0100

    Create hover effect, Add buttons, Resize grid
    The grid items have a permanent hover effect that could be
    erzed with the eraze button and the grid is resizable through
    the reize button

--- script.js
+++ script.js
@@ -1,30 +1,48 @@
 
-
-
-
-
-// creating a grid
+// Create the grid
 const container = document.getElementById("grid");
 function makeRows(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
-    container.appendChild(cell).className = "grid-item";
-    cell.setAttribute('style', 'background-color: blue;'); 
+    container.appendChild(cell).id = "grid-item";
 
-    cell.addEventListener('mouseenter', () => {
-        cell.setAttribute('style', 'background-color: red;');    
-      });
-       
-
+    // ADD permanent hover effect
+    cell.addEventListener('mousemove', () => {
+      cell.setAttribute('style', 'background-color: yellow')
+    });
   };
 };
 
-makeRows(50, 50);
 
 
 
+
+window.onload = makeRows(16, 16);
+
+
+// resize the grid
+const resize = document.getElementById('resize')
+resize.addEventListener('click', () => {
+  let cellCount = prompt("Enter the cell count you want: ");
+  container.innerHTML = ""
+  makeRows(Number(cellCount), Number(cellCount));
+
+});
+
+
+
+
+// console.log(gridItem)
+const gridItem = document.querySelectorAll('#grid-item');
+const eraze = document.getElementById('eraze');
+
+eraze.addEventListener('click', () => {
+  Array.from(gridItem).forEach(item => {
+    item.setAttribute('style', 'background-color: none')
+  });
+});
 
 
 
